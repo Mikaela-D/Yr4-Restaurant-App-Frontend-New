0c23c54ac2d6214c121057eeaca49e9ed037adc1
Mikaela-D <G00401392@atu.ie>
2025-04-09 20:25:23 +0100

    Added add to cart on the backend code

--- contexts/CartContext.js
+++ contexts/CartContext.js
@@ -5,8 +5,28 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  const addToCart = (product) => {
+  const addToCart = async (product) => {
     setCart((prevCart) => [...prevCart, product]);
+
+    try {
+      const res = await fetch("http://127.0.0.1:3010/addToCart", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          productId: product.ourId,
+          name: product.name,
+          price: product.price,
+        }),
+      });
+      const data = await res.json();
+      if (!data.success) {
+        console.error("Failed to save cart item to database:", data.theError);
+      }
+    } catch (err) {
+      console.error("Error adding cart item to database:", err);
+    }
   };
 
   const removeFromCart = (productId) => {
